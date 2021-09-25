05066292bd5374b368ff7439c0637f8646903f07
Jaquie <jaq.cat@protonmail.ch>
2021-09-24 22:19:02 -0300

    sssssssss

--- application/controllers/index.py
+++ application/controllers/index.py
@@ -8,14 +8,24 @@ bp = Blueprint('index', __name__)
 
 @bp.get('/')
 def index():
-    return render_template('index.jinja2', title='Title')
+    return render_template('board.jinja2',
+            title="All",
+            pf=PostForm(),
+            posts=Post.query.all())
 
 @bp.get('/b/<board>')
 def board_posts(board: str):
-    print(Board.query.all())
     return render_template('board.jinja2',
             title=board,
             pf=PostForm(),
-            posts=Post.query.all())
+            posts=Post.query.filter_by(board=board))
+
+@bp.get('/p/<post>')
+def post(post: str):
+    print(Board.query.all())
+    post = Post.query.filter_by(uid=post).first()
+    return render_template('post.jinja2',
+            title=post.title,
+            post=post)
 
 app.register_blueprint(bp)
